c4eaec304a322fd3cfc8de2700a66d141892edbb
readm <lijinfeng@iie.ac.cn>
2017-03-24 09:11:11 +0800

    change path

--- src/unsorted/buffer_overflow_gyj_001/compile.sh
+++ src/unsorted/buffer_overflow_gyj_001/compile.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-gcc -fno-stack-protector -z execstack -g -o stack_overflow stack_overflow.c 
+gcc -fno-stack-protector -z execstack -g -o ./vul/stack_overflow ./vul/stack_overflow.c 
+gcc -o ./check/stack_overflow_check ./check/stack_overflow_check.c
 
